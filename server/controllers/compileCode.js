a5e668ed89d850f221d220edfa2ca7092481f660
Yaswanth Myneni <79623546+yaswanthmyneni@users.noreply.github.com>
2026-04-24 13:02:41 +0530

    feat: if user not given input the it will take from 1st testcases from that problem

---
+++ server/controllers/compileCode.js
@@ -0,0 +1,50 @@
+import Problem from "../models/problem.js";
+import {
+  executeCode,
+  generateFile,
+  generateInputFile,
+} from "../utils/index.js";
+
+const compileCode = async (req, res) => {
+  const { problemId, lang = "cpp", code, input } = req.body;
+
+  if (!code) {
+    return res.status(400).json({ success: false, error: "Empty code!" });
+  }
+
+  try {
+    let finalInput = input;
+
+    if (!finalInput) {
+      if (!problemId) {
+        return res.status(400).json({
+          success: false,
+          error: "No input or problemId provided",
+        });
+      }
+
+      const problem = await Problem.findById(problemId);
+
+      if (!problem || !problem.testcases.length) {
+        return res.status(404).json({
+          success: false,
+          error: "Problem or testcases not found",
+        });
+      }
+
+      finalInput = problem.testcases[0].input;
+    }
+
+    finalInput = finalInput.replace(/\\n/g, "\n");
+
+    const filePath = generateFile(lang, code);
+    const inputFilePath = generateInputFile(finalInput);
+    const output = await executeCode(filePath, inputFilePath);
+
+    res.json({ filePath, output });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+
+export { compileCode };
