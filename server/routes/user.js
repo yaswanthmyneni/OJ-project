14338c21ad1dab45e615752112d4a0c96191a2c9
Yaswanth Myneni <79623546+yaswanthmyneni@users.noreply.github.com>
2026-05-02 20:59:13 +0530

    removed authorization part as it is giving error in ec2 instance, need to fix it.

--- server/routes/user.js
+++ server/routes/user.js
@@ -27,8 +27,8 @@ userRouter.get("/logout", logout);
  * @path /user/dashboard
  */
 // Protected route
-userRouter.get("/dashboard", auth, (req, res) => {
-  res.json({ msg: "Welcome", user: req.user });
+userRouter.get("/dashboard", (req, res) => {
+  res.json({ msg: "Welcome", user: true });
 });
 
 export { userRouter };
